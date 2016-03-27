b0421701cfc5a2f53bb00dc57ff2ef864edd703e
Zaturrby <rjkorteschiel@gmail.com>
2016-03-27 19:46:58 +0200

    change script

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@ bundle exec middleman build
 cp .nojekyll build/.nojekyll
 echo info-staging.offcourse.io > build/CNAME
 cd build
+git init
 git config user.name "Zaturrby"
 git config user.email "rjkorteschiel@gmail.com"
-git init
 git add -A
 git commit -m "deploy"
 git push -f --quiet "https://${GH_TOKEN}@github.com/offcourse/offcourse-info.git" master:gh-pages > /dev/null 2>&1
