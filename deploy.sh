d50044e0f5ebabc10e8bee1196c3834be557a73b
Zaturrby <rjkorteschiel@gmail.com>
2016-03-27 15:51:38 +0200

    change name in deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ cp .nojekyll build/.nojekyll
 cp CNAME build/CNAME
 cd build
 git init
-git remote add origin git@github.com:offcourse/offcourse-documentation-middleman.git
+git remote add origin git@github.com:offcourse/offcourse-info.git
 git add -A
 git commit -m "deploy"
 git checkout -B gh-pages
