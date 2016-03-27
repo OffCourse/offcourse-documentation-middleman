4d8f362ea100f39d627ec8166bfd6b72483fde68
Zaturrby <rjkorteschiel@gmail.com>
2016-03-27 19:38:17 +0200

    add travis secure

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,10 @@ bundle exec middleman build
 cp .nojekyll build/.nojekyll
 echo info-staging.offcourse.io > build/CNAME
 cd build
+git config user.name "Zaturrby"
+git config user.email "rjkorteschiel@gmail.com"
 git init
-git remote add origin git@github.com:offcourse/offcourse-info.git
 git add -A
 git commit -m "deploy"
-git checkout -B gh-pages
+git push -f --quiet "https://${GH_TOKEN}@github.com/offcourse/offcourse-info.git" master:gh-pages > /dev/null 2>&1
 git push -u origin gh-pages -f
