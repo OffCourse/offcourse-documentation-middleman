43bd42c060133811cfcf6a3431cf182f3f145a05
Zaturrby <rjkorteschiel@gmail.com>
2016-03-28 16:08:10 +0200

    change deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-echo "The current branch is:  ${TRAVIS_BRANCH}   "
-if [ $TRAVIS_BRANCH -eq "master"]
+echo "The current branch is:  $TRAVIS_BRANCH   "
+if [ "$TRAVIS_BRANCH" = "master" ]
 then
   echo "Pushing to staging!!!"
-  GH_REF = "github.com/offcourse/offcourse-info.git"
-elif [ $TRAVIS_BRANCH -eq "production" ]
+  export GH_REF="github.com/offcourse/offcourse-info.git"
+elif [ "$TRAVIS_BRANCH" = "production" ]
 then
   echo "Pushing to production!!!"
   GH_REF = "github.com/offcourse/offcourse-info.git"
@@ -15,10 +15,6 @@ cd build
 git init
 git config user.name "Zaturrby"
 git config user.email "rjkorteschiel@gmail.com"
-git status
 git add -A
-git status
 git commit -m "deploy"
-git status
 git push -f --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-git status
