e330055550998aa8bf7d8b2cea718b6bf3dc2eba
Zaturrby <rjkorteschiel@gmail.com>
2016-03-28 17:13:55 +0200

    change

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
-if [ "${TRAVIS_BRANCH}" = "master" ]
+if [ $TRAVIS_BRANCH = "master" ]
 then
   export GH_REF="github.com/offcourse/offcourse-info.git"
-elif [ "${TRAVIS_BRANCH}" = "production" ]
+elif [ $TRAVIS_BRANCH = "production" ]
 then
   GH_REF = "github.com/offcourse/offcourse-info.git"
 fi
+
+# bundle exec middleman build
 
 cp .nojekyll build/.nojekyll
 echo info-staging.offcourse.io > build/CNAME
@@ -14,4 +16,5 @@ git config user.name "Zaturrby"
 git config user.email "rjkorteschiel@gmail.com"
 git add -A
 git commit -m "deploy"
-git push -f --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+# git push -f --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push -f "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
